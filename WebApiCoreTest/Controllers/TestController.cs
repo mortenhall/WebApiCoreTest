513d51daea1b849ed69644460c5550a7a59ea6f1
mortenhall <44943751+mortenhall@users.noreply.github.com>
2018-11-11 13:52:40 +0100

    Summary

---
+++ WebApiCoreTest/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiCoreTest.Controllers
+{
+    [ApiController]
+    [Route("api/test")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string[]> GetNames()
+        {
+            string[] names = { "Morten, Merete, Louis" };
+            return names;
+        }
+    }
+}
